d437acb2838e5a175a56cb7f3c6a3502a5b7451c
Buliet <czhgoi@gmail.com>
2021-09-11 21:33:39 +0800

    perf (pro)  :  improve at current level

--- pro/Calc/calc.js
+++ pro/Calc/calc.js
@@ -2,7 +2,7 @@
 const calc = document.querySelector('.calc');
 const keys = calc.querySelector('.calc_keys');
 const display = calc.querySelector('.calc_display');
-const history = document.querySelector('.calc_history');
+const calc_history = document.querySelector('.calc_history');
 // 属性查询
 const ClearButton = calc.querySelector('[data-action=clear]');
 
@@ -29,8 +29,15 @@ const getKeyType = (key) => {
         action === 'multiply' ||
         action === 'divide'
     ) return 'operator';
+    // clear decimal calculate
     return action;
+}
 
+const initData = (state) => {
+    state.firstValue = '';
+    state.modValue = '';
+    state.operator = '';
+    state.previousKeyType = '';
 }
 const getResult = (key, displayedNum, state) => {
     // .textContent 表示节点的文本内容
@@ -43,33 +50,43 @@ const getResult = (key, displayedNum, state) => {
         modValue,
         previousKeyType
     } = state;
+    var cresult = '';
+    var hresult = '';
 
     if (KeyType === 'number') {
         // 三元运算符
-        return displayedNum === '0' ||
+        cresult = displayedNum === '0' ||
             previousKeyType === 'operator' ||
-            previousKeyType === 'calculate' ||
-            previousKeyType === 'clear' ?
+            previousKeyType === 'calculate' ?
             keyContent :
             displayedNum + keyContent;
+        hresult = keyContent;
 
     }
     if (KeyType === 'decimal') {
         // 判断是否包含
-        if (!displayedNum.includes('.')) return displayedNum + '.';
-        if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.';
-        return displayedNum;
-
+        if (!displayedNum.includes('.')) {
+            cresult = displayedNum + '.';
+            hresult = '.';
+        }
+        if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
+            cresult = '0.';
+            hresult = cresult;
+        } else {
+            cresult = displayedNum;
+            hresult = '';
+        }
     }
 
     if (KeyType === 'operator') {
         // 且操作
-        return firstValue &&
+        cresult = firstValue &&
             operator &&
             previousKeyType !== 'operator' &&
-            previousKeyType !== 'calculate' &&
-            previousKeyType !== 'clear' ?
+            previousKeyType !== 'calculate' ?
+            // ??           && previousKeyType !== 'clear'
             calculate(firstValue, operator, displayedNum) : displayedNum;
+        hresult = keyContent;
         // way-1 update first value correctly
         //     calc.dataset.firstValue = calcValue;
         // } else {
@@ -81,15 +98,22 @@ const getResult = (key, displayedNum, state) => {
 
     }
 
-    if (KeyType === 'clear') return 0;
+    if (KeyType === 'clear') {
+        cresult = 0;
+
+        hresult = key.textContent === 'AC' ? '<br>' : "CE ";
+    }
 
     if (KeyType === 'calculate') {
-        return firstValue && previousKeyType !== 'clear' && previousKeyType !== 'operator' ?
+        cresult = firstValue ?
+            // ??&& previousKeyType !== 'clear' && previousKeyType !== 'operator'
             previousKeyType === 'calculate' ?
             calculate(displayedNum, operator, modValue) :
             calculate(firstValue, operator, displayedNum) :
-            displayedNum
+            displayedNum;
+        hresult = keyContent + cresult + '<br>';
     }
+    return [cresult, hresult]
 }
 
 const updateCalcState = (key, calc, calcValue, displayedNum) => {
@@ -102,6 +126,16 @@ const updateCalcState = (key, calc, calcValue, displayedNum) => {
     } = calc.dataset;
     calc.dataset.previousKeyType = KeyType;
 
+    // origin author miss this
+    if ((KeyType === 'number' || KeyType === 'decimal') &&
+        previousKeyType === 'calculate') {
+        initData(calc.dataset);
+    }
+
+    if (KeyType === 'clear' && key.textContent === 'AC') {
+        initData(calc.dataset);
+    }
+
     if (KeyType === 'operator') {
         calc.dataset.operator = key.dataset.action;
         calc.dataset.firstValue = firstValue &&
@@ -112,20 +146,12 @@ const updateCalcState = (key, calc, calcValue, displayedNum) => {
             displayedNum
     }
 
-
     if (KeyType === 'calculate') {
         calc.dataset.modValue = firstValue && previousKeyType === 'calculate' ?
             modValue :
             displayedNum
     }
-    if (KeyType === 'clear' && key.textContent === 'AC') {
-        calc.dataset.firstValue = '';
-        calc.dataset.modValue = '';
-        calc.dataset.operator = '';
-        calc.dataset.previousKeyType = '';
 
-        history.innerHTML = history.innerHTML + '<br>';
-    }
 }
 
 const updateVisualState = (key, calc) => {
@@ -143,15 +169,20 @@ const updateVisualState = (key, calc) => {
     }
 }
 
+const updateHistory = (result) => {
+    calc_history.innerHTML = calc_history.innerHTML + result;
+}
+
 keys.addEventListener('click', e => {
     // 监听事件 条件匹配元素/类型/id/属性等
     if (!e.target.matches('button')) return
     const key = e.target;
     const displayedNum = display.textContent;
-    const result = getResult(key, displayedNum, calc.dataset);
-    display.textContent = result;
-    updateCalcState(key, calc, result, displayedNum);
-
-    history.innerHTML = history.innerHTML + key.textContent;
+    const resultArr = getResult(key, displayedNum, calc.dataset);
+    cal_result = resultArr[0];
+    his_result = resultArr[1];
+    display.textContent = cal_result;
+    updateCalcState(key, calc, cal_result, displayedNum);
     updateVisualState(key, calc);
+    updateHistory(his_result);
 });
--- pro/GuessTheColor/script.js
+++ pro/GuessTheColor/script.js
@@ -1,7 +1,31 @@
 var resetButton = document.getElementById('resetButton');
 resetButton.addEventListener("click", StartGame);
+var radio = document.getElementsByName('GType');
+radio.forEach(e => {
+    e.addEventListener("click", StartGame);
 
-function setButtonbcolor(button, red, green, blue) {
+});
+var words = ["Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Purple", "Black", "White"]
+
+function getGameType() {
+    // forEach()方法不返回值，只用来操作数据
+    // 如果数组遍历的目的是为了得到返回值， 那么使用map() 方法， 否则使用forEach() 方法
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked)
+            return radio[i].value;
+    }
+}
+
+// 获取元素的颜色
+function getButtonColor(button) {
+    return button.style.color;
+}
+
+function getButtonBColor(button) {
+    return button.style.backgroundColor;
+}
+
+function setButtonBcolor(button, red, green, blue) {
     // 设置元素属性
     button.setAttribute('style',
         // 背景颜色
@@ -16,28 +40,56 @@ function setButtoncolor(button, red, green, blue) {
     );
 }
 
+function setButtonWBcolor(button, color) {
+    // 设置元素属性
+    button.setAttribute('style',
+        // 背景颜色
+        'background-color: ' + color + ';'
+    );
+}
+
+function setButtonWcolor(button, color) {
+    button.setAttribute('style',
+        // 文本颜色
+        'color: ' + color + ';'
+    );
+}
+
 function getRandomInt(max) {
     // 获取 0-max 随机整数 (四舍五入)
     return Math.round(Math.random() * max);
 }
 
-function changecolor(button, type = 0) {
+function changecolor(button, type = 0, GType = 'Color') {
     red = getRandomInt(255);
     green = getRandomInt(255);
     blue = getRandomInt(255);
-    if (type == 0) {
-        setButtonbcolor(button, red, green, blue);
+    selected = getRandomInt(words.length - 1);
+    if (GType === 'Color') {
 
+        if (type == 0) {
+            setButtonBcolor(button, red, green, blue);
+
+        } else {
+            setButtoncolor(button, red, green, blue);
+
+        }
+        return [red, green, blue]
     } else {
-        setButtoncolor(button, red, green, blue);
+        if (type == 0) {
+            setButtonWBcolor(button, words[selected]);
 
+        } else {
+            setButtonWcolor(button, words[selected]);
+        }
+        return words[selected];
     }
-    return [red, green, blue]
 }
 
 function StartGame() {
     var heading = document.getElementById('colorValue');
     var answerMessage = document.getElementById('answer');
+    var GType = getGameType();
     // 响应内部设置为空
     answerMessage.innerHTML = '';
 
@@ -45,13 +97,21 @@ function StartGame() {
     var answerButton = getRandomInt(buttons.length - 1);
 
     for (var i = 0; i < buttons.length; i++) {
-        colors = changecolor(buttons[i], 0);
+        colors = changecolor(buttons[i], 0, GType);
         if (i == answerButton) {
-            heading.innerHTML = `(${colors[0]},${colors[1]},${colors[2]})`
-            setButtoncolor(heading, colors[0], colors[1], colors[2]);
+            if (GType === 'Color') {
+                heading.innerHTML = `(${colors[0]},${colors[1]},${colors[2]})`
+                setButtoncolor(heading, colors[0], colors[1], colors[2]);
+            } else {
+                heading.innerHTML = colors;
+                setButtonWcolor(heading, colors);
+                var rcolor = getButtonColor(heading);
+            }
         }
+        // 增加颜色值判断 是否正确
         buttons[i].addEventListener('click', function() {
-            if (this === buttons[answerButton]) {
+            var ccolor = getButtonBColor(this);
+            if (this === buttons[answerButton] || ccolor === rcolor) {
                 answerMessage.innerHTML = "Correct!";
                 // TODO change body's backgound color too
             } else {
